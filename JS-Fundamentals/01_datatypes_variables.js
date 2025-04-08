143b910576839c91fb2de278b52ede106726f1d7
Fagan Parte <faganparte1004@gmail.com>
2025-04-08 11:59:38 +0530

    if else

---
+++ JS-Fundamentals/01_datatypes_variables.js
@@ -0,0 +1,49 @@
+/*console.log("Hello My name is Fagan Parte")
+console.log("Who Am I")
+console.log("DDUGKY")*/
+//1.string,"Fagan","21","true"
+//2.Number,21
+//3.BigInt
+//4.Boolean,true/false
+//5.Undifined
+//6.Null
+//Symbol
+
+/////////////////////
+//02.  Variables
+///////////////////
+
+
+//Identifiers/Name And Literals/Value
+/*var firstName ="Fagan";
+var lastName = "Parte";
+var age = "21";
+var married =false;
+var wifeName ; //Undifined
+var thirdName = null;*/
+/*var firstName ="fagan";
+var lastName ="parte";
+var age =21;
+console.log(firstName,lastName);
+//Print:"Good Morning Fagan Parte!"
+console.log("Good Morning",firstName,lastName,"!");
+console.log("Hi,My name is", firstName, lastName,"and my age is",age,".");*/
+/*var firstName = "Ram"
+var lastName = "Parte"
+conasole.log("Goodmornig",firstName,lastName, "!")*/
+var marks = 65;
+var grad;
+
+var conditionA = (marks>=90)
+var conditionB = (marks>=70 && marks<=90)
+var conditionC = (marks>=60 && marks<=70)
+
+
+if (conditionA) grad = "A";
+else if(conditionB) grad = "B";
+else if(conditionC) grad = "C";
+
+console.log(grad)
+
+
+
