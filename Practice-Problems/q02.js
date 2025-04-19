f6987864698fb20f73224d9778182bfaa6df0ebf
Fagan Parte <faganparte1004@gmail.com>
2025-04-19 20:42:18 +0530

    Practice-Problems

--- Practice-Problems/q02.js
+++ Practice-Problems/q02.js
@@ -1,10 +1,23 @@
-let digit = 94560;
-let pointer = 0;
-while(digit<=pointer){
-    if(digit%10)
-        console.log(digit)
-    digit++;
-}
+ //print even numbers 1 to 20
+ let num = 1;
+ let max = 20
+ while(num<=max){
+    if(num%2)
+ }
+
+
+
+     
+
+
+
+
+
+
+
+
+
+  
     
     
 
